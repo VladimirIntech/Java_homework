bfcff4803fc4fcf248ab29b73b5ac65947402167
VladimirIntech <44566678+VladimirIntech@users.noreply.github.com>
2021-11-25 18:07:33 +0300

    лекция 1.8

--- sandbox/src/main/java/ru/javahomework/sandbox/FirstVersion.java
+++ sandbox/src/main/java/ru/javahomework/sandbox/FirstVersion.java
@@ -3,12 +3,15 @@ package ru.javahomework.sandbox;
 public class FirstVersion {
   public static void main(String[] args) {
     hello("world");
-    double l =5;
-    System.out.println("Площадь квадрата со стороной " +l+ " = "+ area(l));
+    hello("User");
 
-    double a = 4;
-    double b = 6;
-    System.out.println("Площадь прямоугольника со сторонами " +a+ " и "+b+" = "+ area(a,b));
+
+    Square s = new Square(5);
+
+    System.out.println("Площадь квадрата со стороной " +s.l+ " = "+ area(s));
+
+    Rectangle r = new Rectangle(4,6);
+    System.out.println("Площадь прямоугольника со сторонами " +r.a+ " и "+r.b+" = "+ area(r));
   }
 
   public static void hello(String somebody) {
@@ -16,10 +19,10 @@ public class FirstVersion {
     System.out.println("Hello, " + somebody);
   }
 
-  public static double area(double l) {
-    return l * l;
-  }
-  public static double area(double a, double b) {
-    return a * b;
+  public static double area(Square s) {
+    return s.l* s.l;
+ }
+  public static double area(Rectangle r) {
+    return r.a * r.b;
   }
 }
