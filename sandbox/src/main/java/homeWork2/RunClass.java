96b6b88f257979ad8d74f9e7b884d4e63fcfacfd
VladimirIntech <44566678+VladimirIntech@users.noreply.github.com>
2021-11-26 11:02:21 +0300

    Изменено 1 е задание. Добавлено второе задание

---
+++ sandbox/src/main/java/homeWork2/RunClass.java
@@ -0,0 +1,10 @@
+package homeWork2;
+
+public class RunClass {
+  public static void main(String[] args) {
+    Point p1 = new Point(2, 2,10,8);
+    System.out.println("Расстояние между точками (" + p1.x1 +","+p1.y1+") и (" +p1.x2 +","+ p1.y2 + ") = " + p1.distance());
+  }
+}
+
+
