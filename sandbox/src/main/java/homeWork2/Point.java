48b8fd9a675a8024bfddf339b11261c1b2893eea
VladimirIntech <44566678+VladimirIntech@users.noreply.github.com>
2021-11-26 12:15:32 +0300

    Доработано задание 2

--- sandbox/src/main/java/homeWork2/Point.java
+++ sandbox/src/main/java/homeWork2/Point.java
@@ -15,7 +15,7 @@ public class Point {
   }
   public double distance() {
 
-    double d = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 -y1) * (y2 * y1));
+    double d = Math.sqrt((this.x2 - this.x1) * (this.x2 - this.x1) + (this.y2 - this.y1)*(this.y2 - this.y1));
     return d;
 
   }
--- sandbox/src/main/java/homeWork2/RunClass.java
+++ sandbox/src/main/java/homeWork2/RunClass.java
@@ -2,8 +2,8 @@ package homeWork2;
 
 public class RunClass {
   public static void main(String[] args) {
-    Point p1 = new Point(2, 2,10,8);
-    System.out.println("Расстояние между точками (" + p1.x1 +","+p1.y1+") и (" +p1.x2 +","+ p1.y2 + ") = " + p1.distance());
+    Point p = new Point(2, 2,10,8);
+    System.out.println("Расстояние между точками (" + p.x1 +","+p.y1+") и (" +p.x2 +","+ p.y2 + ") = " + p.distance());
   }
 }
 
