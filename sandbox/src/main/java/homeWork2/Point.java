9fe355288478a0c295f0d436bf601ac3c762aaac
VladimirIntech <44566678+VladimirIntech@users.noreply.github.com>
2021-11-29 17:27:06 +0300

    задание 2 переделано

--- sandbox/src/main/java/homeWork2/Point.java
+++ sandbox/src/main/java/homeWork2/Point.java
@@ -2,22 +2,17 @@ package homeWork2;
 
 public class Point {
 
-  public double x1;
-  public double y2;
-  public double y1;
-  public double x2;
+  public double x;
+  public double y;
 
-  public Point (double x1, double y1, double x2, double y2) {
-    this.x1 = x1;
-    this.y1 = y1;
-    this.x2 = x2;
-    this.y2 = y2;
+  public Point  (double x1, double y1) {
+    this.x = x1;
+    this.y = y1;
   }
-  public double distance() {
 
-    double d = Math.sqrt((this.x2 - this.x1) * (this.x2 - this.x1) + (this.y2 - this.y1)*(this.y2 - this.y1));
-    return d;
-
+  public double distance(Point p2){
+    double sqrt = Math.sqrt((p2.x - this.x) * (p2.x - this.x) + (p2.y - this.y) * (p2.y - this.y));
+    return sqrt;
   }
 
 
--- sandbox/src/main/java/homeWork2/RunClass.java
+++ sandbox/src/main/java/homeWork2/RunClass.java
@@ -2,8 +2,10 @@ package homeWork2;
 
 public class RunClass {
   public static void main(String[] args) {
-    Point p = new Point(2, 2, 10, 8);
-    System.out.println("Расстояние между точками (" + p.x1 + "," + p.y1 + ") и (" + p.x2 + "," + p.y2 + ") = " + p.distance());
+    Point p = new Point(2, 2);
+    Point p2 = new Point(10, 8);
+    System.out.println("Расстояние между точками (" + p.x + " , " + p.y + " ) и ( " + p2.x + " , " + p2.y + ") = " + p.distance(p2));
+
   }
 }
 
