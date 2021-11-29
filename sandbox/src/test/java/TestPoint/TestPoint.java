ed3ca682147f29a76bb66541c9431e5976410214
VladimirIntech <44566678+VladimirIntech@users.noreply.github.com>
2021-11-29 17:37:43 +0300

    Переделано 3 е задание изменен вызов метода по замечаниям

--- sandbox/src/test/java/TestPoint/TestPoint.java
+++ sandbox/src/test/java/TestPoint/TestPoint.java
@@ -7,27 +7,31 @@ import org.testng.annotations.Test;
 public class TestPoint {
   @Test
   public void testP() {
-    Point p = new Point(1,3,22,11);
-    Assert.assertEquals(p.distance(), 		22.47220505424423);
+    Point p = new Point(1,3);
+    Point p2 = new Point(22,11);
+    Assert.assertEquals(p.distance(p2), 		22.47220505424423);
 
 
   }
   @Test
   public void testP2() {
-    Point p = new Point(1,3,22,11);
-    Assert.assertNotEquals(p.distance(), 25);
+    Point p = new Point(1,3);
+    Point p2 = new Point(22,11);
+    Assert.assertNotEquals(p.distance(p2), 25);
 
   }
   @Test
   public void testP3() {
-    Point p = new Point(10,333,22,11);
-    Assert.assertEquals(p.distance(), 322.2235249015813);
+    Point p = new Point(10,333);
+    Point p2 = new Point(22,11);
+    Assert.assertEquals(p.distance(p2), 322.2235249015813);
 
   }
   @Test
   public void testP4() {
-    Point p = new Point(10,333,22,111);
-    Assert.assertEquals(p.distance(), 322.2235249015813);
+    Point p = new Point(10,333);
+    Point p2 = new Point(22,111);
+    Assert.assertEquals(p.distance(p2), 322.2235249015813);
 
   }
 }
